aa08fd42e51f663b6ba4f81ff4d44a04cb117829
Bin Xu <xubinrun@gmail.com>
2022-09-05 22:28:34 +0800

    force to untils for new version

--- compute_voro_vec_atom.cpp
+++ compute_voro_vec_atom.cpp
@@ -30,7 +30,7 @@
 #include "comm.h"
 #include "variable.h"
 #include "input.h"
-#include "force.h"
+//#include "force.h"
 
 #include <vector>
 
@@ -101,16 +101,16 @@ ComputeVoroVec::ComputeVoroVec(LAMMPS *lmp, int narg, char **arg) :
       iarg += 2;
     } else if (strcmp(arg[iarg], "edge_histo") == 0) {
       if (iarg + 2 > narg) error->all(FLERR,"Illegal compute voronoi/atom command");
-      maxedge = force->inumeric(FLERR,arg[iarg+1]);
+      maxedge = utils::inumeric(FLERR,arg[iarg+1]);
       size_peratom_cols += 4;
       iarg += 2;
     } else if (strcmp(arg[iarg], "face_threshold") == 0) {
       if (iarg + 2 > narg) error->all(FLERR,"Illegal compute voronoi/atom command");
-      fthresh = force->numeric(FLERR,arg[iarg+1]);
+      fthresh = utils::numeric(FLERR,arg[iarg+1]);
       iarg += 2;
     } else if (strcmp(arg[iarg], "edge_threshold") == 0) {
       if (iarg + 2 > narg) error->all(FLERR,"Illegal compute voronoi/atom command");
-      ethresh = force->numeric(FLERR,arg[iarg+1]);
+      ethresh = utils::numeric(FLERR,arg[iarg+1]);
       iarg += 2;
     } else if (strcmp(arg[iarg], "neighbors") == 0) {
       if (iarg + 2 > narg) error->all(FLERR,"Illegal compute voronoi/atom command");
